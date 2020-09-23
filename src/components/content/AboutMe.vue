19462aaab06f31c2abc090697e6a5164a62c3412
Sandro <PSandro@users.noreply.github.com>
2020-09-23 19:18:00 +0200

    Update AboutMe.vue

--- src/components/content/AboutMe.vue
+++ src/components/content/AboutMe.vue
@@ -2,7 +2,7 @@
 <div class="box">
   <img id="profile" alt="Image of myself" src="./../../assets/profile.jpg">
   <p id="text">My Name is Sandro Pischinger and a big part of my life is my passion for technology: Servers, Computers, Coding, DevOps, IT-Security, Cables, Buttons. These words are music to my ears! 
-My personal experience and interest in technology has grown for over a decade now - with me just being 18 years old. So naturally I've always been the one to ask, when any tech related devices - the computer/TV/projector/speakers - weren´t working the way they should in class or anywhere else for that matter. In addition to just using technology, I early began creating and modifying stuff, assembling computers and wiring LAN-networks. 
+My personal experience and interest in technology has grown for over a decade now - with me just being 19 years old. So naturally I've always been the one to ask, when any tech related devices - the computer/TV/projector/speakers - weren´t working the way they should in class or anywhere else for that matter. In addition to just using technology, I early began creating and modifying stuff, assembling computers and wiring LAN-networks. 
 Having gained a decent knowledge about hardware over the years, I soon started working with software for example by contributing to OpenSource-Software, creating websites and handy Bash or Python scripts, as well as creating applications with other tech enthusiasts at a coding-camp. 
 Furthermore I have an insatiable interest in learning new things and working with the latest technology!</p>
 </div>
